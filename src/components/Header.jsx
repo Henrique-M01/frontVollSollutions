7494eab5a1260de0a7a8d0eb8ef854b0ee7b7451
henrique <hickm018@gmail.com>
2022-05-15 17:10:50 -0300

    feat: add style header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,20 @@
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import MyContext from '../Context/MyContext';
+import '../style/Header.css';
 
 export default function Header() {
   const { role, name, coins } = useContext(MyContext);
 
   return (
-    <div>
+    <header className="header-container">
       {role === 'admin' &&
-      <Link to="/edit">Editar usuarios</Link>}
+      <Link to="/edit" className="link-edit">Editar usuarios</Link>}
       <h1>Products</h1>
-      <h2>{ name }</h2>
-      <h3>{ coins }</h3>
-    </div>
+      <div className="name-and-coins">
+        <p className="name-p">{ name }</p>
+        <p>Suas moedas: { coins }</p>
+      </div>
+    </header>
   )
 }
